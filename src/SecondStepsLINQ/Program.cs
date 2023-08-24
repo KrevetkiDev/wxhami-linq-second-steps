b974239f8e506811b3acdea0da1ac80361a43db5
wxhami <d18102002@mail.ru>
2023-08-24 12:46:22 +0300

    TaskTwo done

--- src/SecondStepsLINQ/Program.cs
+++ src/SecondStepsLINQ/Program.cs
@@ -1,4 +1,6 @@
-﻿void TaskOne()
+﻿using System.ComponentModel.Design;
+
+void TaskOne()
 {
     List<int> listForSortt = new List<int>() { 1, 3, 5, 10 };
     List<int> sortedListt = new List<int>();
@@ -18,6 +20,24 @@
         }
         return sortedList;
     }
-    
-    
+}
+
+void TaskTwo()
+{
+    List<int> listForSort = new List<int>() { 1, 3, 5, 10 };
+
+    var firtNumber = listForSort.FirstOrDefault(number => number == 4);
+
+    int GetElement(List<int> list, int number)
+    {
+        foreach (var numb in list)
+                {
+                    if (numb == number)
+                    {
+                        return numb;
+                    }
+                }
+
+        return default;
+    }
 }
