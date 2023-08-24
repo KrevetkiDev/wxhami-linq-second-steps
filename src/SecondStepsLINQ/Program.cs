819fd3b45979bcdfa57cca2061a596992a45b048
wxhami <d18102002@mail.ru>
2023-08-24 12:00:13 +0300

    TaskOne done

--- src/SecondStepsLINQ/Program.cs
+++ src/SecondStepsLINQ/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿void TaskOne()
+{
+    List<int> listForSortt = new List<int>() { 1, 3, 5, 10 };
+    List<int> sortedListt = new List<int>();
 
-Console.WriteLine("Hello, World!");
+    var num = listForSortt.Where(number => number > 3);
+    
+    List<int> Sort(List<int> listForSort, List<int> sortedList)
+    {
+        listForSort = listForSortt;
+        sortedList = sortedListt;
+        foreach (var number in listForSort)
+        {
+            if (number > 3)
+            {
+                sortedList.Add(number);
+            }
+        }
+        return sortedList;
+    }
+    
+    
+}
